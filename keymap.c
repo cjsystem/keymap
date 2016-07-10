1e858d35c88a307285fa99ecedf248752e9bbd1f
cjsystem <crazyjim.zyh@gmail.com>
2016-07-10 16:36:33 +0900

    change

--- keymap.c
+++ keymap.c
@@ -21,11 +21,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |LGui  |//Ctr|ALT| 6 | Space|                                       |   [  |   ]  |   7  |   ]  | ~L1  |
  *   `----------------------------------'                                `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | PgUp | PgDN |       | LEFT  |RIGHT|
+ *                                        | HOME | END  |       | LEFT  |RIGHT|
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | HOME |       | UP   |        |      |
+ *                                 |      |      | PGUP |       | UP   |        |      |
  *                                 | Enter|Backsp|------|       |------|  DEL   |Enter |
- *                                 |      |ace   | END  |       | DOWN |        |      |
+ *                                 |      |ace   | PGDN  |      | DOWN |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_BSPC,        ALT_T(KC_A),         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         KC_LGUI,CTL_T(KC_SLSH),      KC_LALT,  KC_6,KC_SPC,
-                                                KC_PGUP,  KC_PGDN,
-                                                              KC_HOME,
-                                               KC_ENT,KC_BSPC,KC_END,
+                                                KC_HOME,  KC_END,
+                                                              KC_PGUP,
+                                               KC_ENT,KC_BSPC,KC_PGDN,
         // right hand
              KC_7,     KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL,   LT(SYMB,KC_GRV),
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
